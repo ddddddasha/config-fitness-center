010053c1a930679988e86b957db125d488028528
ddddddasha <daria.zhelobkovich@gmail.com>
2023-09-10 21:16:44 +0300

    update config

--- src/main/java/app/config/fitness/centre/ConfigFitnessApplication.java
+++ src/main/java/app/config/fitness/centre/ConfigFitnessApplication.java
@@ -2,9 +2,11 @@ package app.config.fitness.centre;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ConfigFitnessApplication
 {
